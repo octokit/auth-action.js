7bf2f3132631e6a80efc77cd9d92be0c53f9418f
Gregor Martynus <gregor@martynus.net>
2019-08-16 14:57:51 -0700

    test: initial version

---
+++ test/index.test.ts
@@ -0,0 +1,78 @@
+import { request } from "@octokit/request";
+import fetchMock, { MockMatcherFunction } from "fetch-mock";
+
+import { createActionAuth } from "../src/index";
+
+afterEach(() => {
+  delete process.env.GITHUB_ACTION;
+  delete process.env.GITHUB_TOKEN;
+});
+
+test("README example", async () => {
+  process.env.GITHUB_ACTION = "my-action";
+  process.env.GITHUB_TOKEN = "v1.1234567890abcdef1234567890abcdef12345678";
+
+  const auth = createActionAuth();
+  const authentication = await auth();
+
+  expect(authentication).toEqual({
+    type: "token",
+    token: "v1.1234567890abcdef1234567890abcdef12345678",
+    tokenType: "installation"
+  });
+});
+
+test("GITHUB_ACTION not set", async () => {
+  try {
+    const auth = createActionAuth();
+    throw new Error("Should not resolve");
+  } catch (error) {
+    expect(error.message).toMatch(
+      /\[@octokit\/auth-action\] `GITHUB_ACTION` environment variable is not set/i
+    );
+  }
+});
+
+test("GITHUB_TOKEN not set", async () => {
+  process.env.GITHUB_ACTION = "my-action";
+
+  try {
+    const auth = createActionAuth();
+    throw new Error("Should not resolve");
+  } catch (error) {
+    expect(error.message).toMatch(
+      /\[@octokit\/auth-action\] `GITHUB_TOKEN` environment variable is not set/i
+    );
+  }
+});
+
+test('auth.hook(request, "GET /user")', async () => {
+  process.env.GITHUB_ACTION = "my-action";
+  process.env.GITHUB_TOKEN = "v1.1234567890abcdef1234567890abcdef12345678";
+
+  const expectedRequestHeaders = {
+    accept: "application/vnd.github.v3+json",
+    authorization: "token v1.1234567890abcdef1234567890abcdef12345678",
+    "user-agent": "test"
+  };
+
+  const matchGetUser: MockMatcherFunction = (url, { body, headers }) => {
+    expect(url).toEqual("https://api.github.com/user");
+    expect(headers).toStrictEqual(expectedRequestHeaders);
+    return true;
+  };
+
+  const requestMock = request.defaults({
+    headers: {
+      "user-agent": "test"
+    },
+    request: {
+      fetch: fetchMock.sandbox().getOnce(matchGetUser, { id: 123 })
+    }
+  });
+
+  const { hook } = createActionAuth();
+  const { data } = await hook(requestMock, "GET /user");
+
+  expect(data).toStrictEqual({ id: 123 });
+});
