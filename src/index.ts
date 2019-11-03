f4998a4f60d80d63af7ced1a3426312b779376fb
Gregor Martynus <gregor@martynus.net>
2019-11-02 22:27:51 -0700

    feat(typescript): export with StrategyInterface, export Types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-import { createTokenAuth } from "@octokit/auth-token";
+import { createTokenAuth, Types } from "@octokit/auth-token";
 
-export function createActionAuth() {
+import { StrategyInterface } from "@octokit/types";
+
+export type Types = {
+  StrategyOptions: never;
+  AuthOptions: never;
+  Authentication: Types["Authentication"];
+};
+
+export const createActionAuth: StrategyInterface<
+  [],
+  [],
+  Types["Authentication"]
+> = function createActionAuth() {
   if (!process.env.GITHUB_ACTION) {
     throw new Error(
       "[@octokit/auth-action] `GITHUB_ACTION` environment variable is not set. @octokit/auth-action is meant to be used in GitHub Actions only."
@@ -14,4 +26,4 @@ export function createActionAuth() {
   }
 
   return createTokenAuth(process.env.GITHUB_TOKEN);
-}
+};
